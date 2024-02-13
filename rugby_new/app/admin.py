43c55af0b4b53e7203a71525d05499ce5ac932fd
Pierre Marboutin <piballz@gmail.com>
2024-02-13 11:53:56 +0100

    test-commmit2

--- rugby_new/app/admin.py
+++ rugby_new/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from app.models import Player, ODS
+from app.models import Player, ODS, ODS_lic
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(ODS)
+admin.site.register(ODS_lic)
