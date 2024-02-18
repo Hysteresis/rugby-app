d393473b97b4504c43c59e6f4ecd446476134b6c
Pierre Marboutin <piballz@gmail.com>
2024-02-18 08:36:25 +0100

    added number federation and number geographie in api get method()

--- rugby_new/api/views.py
+++ rugby_new/api/views.py
@@ -3,9 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
-from app.models import F_Club
+from app.models import F_Club, D_Federation, D_Geographie
 from app import serializers
-from app.serializers import FClubSerializer
+from app.serializers import FClubSerializer, DFederationSerializer
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -26,19 +26,29 @@ class EndPointClub(APIView):
     def get(self, request):
         clubs = F_Club.objects.all()
         num_clubs = clubs.count()
+
+        federations = D_Federation.objects.all()
+        num_federations = federations.count()
+        print(num_federations)
+
+        geographies = D_Geographie.objects.all()
+        num_geographies = geographies.count()
+
         serializer = FClubSerializer(clubs, many=True)
         data = {
             'num_clubs': num_clubs,
+            'num_federations': num_federations,
+            'num_geographies': num_geographies,
             'clubs': serializer.data
         }
 
         return Response(data)
 
 
-    def post(self, request):
-        data = request.data
-        result = {
-            'message': 'POST a fonctionné',
-            'data': data
-        }
-        return Response(result, status=status.HTTP_201_CREATED)
+    # def post(self, request):
+    #     data = request.data
+    #     result = {
+    #         'message': 'POST a fonctionné',
+    #         'data': data
+    #     }
+    #     return Response(result, status=status.HTTP_201_CREATED)
--- rugby_new/app/serializers.py
+++ rugby_new/app/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-from app.models import F_Club
+from app.models import F_Club, D_Federation
 
 
 class FClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = F_Club
+        fields = '__all__'
+
+
+class DFederationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = D_Federation
         fields = '__all__'
 
